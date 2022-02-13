6866bc5f45ce6f996e00f51fa462f448707d22cd
Alex Top <alexandru.tp@gmail.com>
2022-02-13 17:59:37 +0200

    Made comments more general

--- ModelBlocks.py
+++ ModelBlocks.py
@@ -16,25 +16,25 @@ class DownscaleBlock(layers.Layer):
         self.pool = layers.MaxPool2D((2, 2), (2, 2))
 
     def call(self, input_tensor):
-        #* input_tensor - (6, 128, 256, 3)
+        #* input_tensor - (6, x, y, z)
         d = self.convA(input_tensor)
-        #* d - (6, 128, 256, 16)
+        #* d - (6, x, y, filters)
         x = self.bn2A(d)
-        #* x - (6, 128, 256, 16)
+        #* x - (6, x, y, filters)
         x = self.reluA(x)
-        #* x - (6, 128, 256, 16)
+        #* x - (6, x, y, filters)
 
         x = self.convB(x)
-        #* x - (6, 128, 256, 16)
+        #* x - (6, x, y, filters)
         x = self.bn2B(x)
-        #* x - (6, 128, 256, 16)
+        #* x - (6, x, y, filters)
         x = self.reluB(x)
-        #* x - (6, 128, 256, 16)
+        #* x - (6, x, y, filters)
 
         x += d
-        #* x - (6, 128, 256, 16)
+        #* x - (6, x, y, filters)
         p = self.pool(x)
-        #* p - (6, 64, 128, 16)
+        #* p - (6, x/2, y/2, filters)
         return x, p
 
 
@@ -53,25 +53,25 @@ class UpscaleBlock(layers.Layer):
         self.conc = layers.Concatenate()
 
     def call(self, x, skip):
-        #* x - (6, 8, 16, 256)
+        #* x - (6, x, y, z)
         x = self.us(x)
-        #* x - (6, 16, 32, 256)
-        #* skip - (6, 16, 32, 128)
+        #* x - (6, x * 2, y * 2, z)
+        #* skip - (6, x * 2, y * 2, w)
         concat = self.conc([x, skip])
-        #* x - (6, 16, 32, 384)
+        #* x - (6, x * 2, y * 2, z + w)
         x = self.convA(concat)
-        #* x - (6, 16, 32, 128)
+        #* x - (6, x * 2, y * 2, filters)
         x = self.bn2A(x)
-        #* x - (6, 16, 32, 128)
+        #* x - (6, x * 2, y * 2, filters)
         x = self.reluA(x)
-        #* x - (6, 16, 32, 128)
+        #* x - (6, x * 2, y * 2, filters)
 
         x = self.convB(x)
-        #* x - (6, 16, 32, 128)
+        #* x - (6, x * 2, y * 2, filters)
         x = self.bn2B(x)
-        #* x - (6, 16, 32, 128)
+        #* x - (6, x * 2, y * 2, filters)
         x = self.reluB(x)
-        #* x - (6, 16, 32, 128)
+        #* x - (6, x * 2, y * 2, filters)
 
         return x
 
@@ -87,14 +87,14 @@ class BottleNeckBlock(layers.Layer):
         self.reluB = layers.LeakyReLU(alpha=0.2)
 
     def call(self, x):
-        #* x - (6, 8, 16, 128)
+        #* x - (6, x, y, z)
         x = self.convA(x)
-        #* x - (6, 8, 16, 256)
+        #* x - (6, x, y, filters)
         x = self.reluA(x)
-        #* x - (6, 8, 16, 256)
+        #* x - (6, x, y, filters)
         x = self.convB(x)
-        #* x - (6, 8, 16, 256)
+        #* x - (6, x, y, filters)
         x = self.reluB(x)
-        #* x - (6, 8, 16, 256)
+        #* x - (6, x, y, filters)
         
         return x
